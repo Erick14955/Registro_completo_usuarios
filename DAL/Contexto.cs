e2ecad467bc449d88e879448ab5139b24bfaf0e6
Erickson Fana Morales <erickson_fana@ucne.edu.do>
2021-02-04 14:14:40 -0400

    Creando la clase roles en la carpetas entidades y BLL. Closes #10.

--- DAL/Contexto.cs
+++ DAL/Contexto.cs
@@ -11,6 +11,8 @@ namespace Proyecto_Registro.DAL
     public class Contexto : DbContext
     {
         public DbSet<Usuarios> Usuarios{ get; set; }
+
+        public DbSet<Roles> Roles { get; set; }
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder.UseSqlite("Data Source = datosUsuarios.Db");
---
+++ Entidades/Roles.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proyecto_Registro.Entidades
+{
+    public class Roles
+    {
+        public int RolID { get; set; }
+
+        public DateTime FechaIngreso { get; set; }
+
+        public string Descripcion { get; set; }
+    }
+}
